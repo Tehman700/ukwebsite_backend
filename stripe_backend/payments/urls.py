e084161e59d68236af85fa3572cd43a06286b75d
Tehman700 <tehmanfast@gmail.com>
2025-09-12 21:35:58 +0500

    Initial Django project with Stripe integration

--- stripe_backend/payments/urls.py
+++ stripe_backend/payments/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("create-checkout-session/", views.create_checkout_session),
+    path("create-checkout-session/", views.create_checkout_session)
 ]
--- stripe_backend/stripe_backend/urls.py
+++ stripe_backend/stripe_backend/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('payments.urls')),  # frontend will call /api/create-checkout-session
+    path('api/', include('payments.urls'))
 ]
